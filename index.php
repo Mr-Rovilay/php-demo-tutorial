684c065905a982e5e5db31b48029de641a38e439
ayodeji akinola <ayodejiakinola007@gmail.com>
2024-11-20 02:43:18 +0100

    commit 2

--- index.php
+++ index.php
@@ -1,18 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./styles/main.css">    
-    <link rel="icon" href="./images/3705085.png" type="image/x-icon">
-    <script src="./script/index.js" defer></script>
-    <title>A simple PHP site</title>
-</head>
+
+<?php
+$title = "Top Trucks";
+include './include/head.inc.php';
+?>
+
 <body>
-    <h1 class="title">Welcome to the website</h1>
+    <!-- navigation -->
+    <?php
+    include './include/navigation.inc.php';
+    ?>
+    <!-- hero -->
+    <section class="hero-container">
+        <div class="hero-text">
+            <h1>Top Trucks</h1>
+            <h2>We buy & sell the best trucks</h2>
+            <a href="contact.php">
+                <button>Contact Us</button>
+            </a>
+        </div>
+    </section>
+    <!-- truck inventory -->
+    <h2 class="inventory-header" id="inventory">Truck Inventory</h2>
+    <div class="trucks-grid">
+
+        <?php 
+        include './include/trucks.inc.php';
+
+        foreach ($truckInventory as $truck) {
+            echo "
+            <div class='truck' data-id='{$truck[0]}'>
+                <img src='{$truck[4]}' alt='{$truck[1]}' loading='lazy'>
+                <div>
+                    <h2>{$truck[1]}</h2>
+                    <p>Price: \${$truck[2]}</p>
+                    <p>Mileage: {$truck[3]}</p>
+                    <a href='contact.php'>
+                        <button>Learn More</button>
+                    </a>
+                </div>
+            </div>
+            ";
+        }
+        ?>
+    </div>
+
+    <!-- footer -->
+    <?php 
+    include './include/footer.inc.php';
+    ?>
+
 </body>
+
 </html>
