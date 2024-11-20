c3c3938a4a579d7e0567c3cee4362fb63ff2c2f1
ayodeji akinola <ayodejiakinola007@gmail.com>
2024-11-20 02:20:08 +0100

    my php commit

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles/main.css">    
+    <link rel="icon" href="./images/3705085.png" type="image/x-icon">
+    <script src="./script/index.js" defer></script>
+    <title>A simple PHP site</title>
 </head>
 <body>
     <h1 class="title">Welcome to the website</h1>
